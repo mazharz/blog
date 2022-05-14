531817a331a80a3e512194fbfb8e6185f389e25d
m47h4r <m47h4r@gmail.com>
2022-05-14 13:12:53 +0430

    feat(blog): add sort by filename

--- lib/helper/blog/blog.ts
+++ lib/helper/blog/blog.ts
@@ -1,28 +1,37 @@
 import path from "path";
 import { marked } from "marked";
 import { readdir, readFile } from "fs/promises";
-import { split, map, head, compose } from "ramda";
+import { split, map, compose, sortWith, nth, descend } from "ramda";
 import { BlogEntry } from "@/Lib/types/blog";
 import { getBlogTitleByEntry } from "./utils";
 
 const BLOG_DIRECTORY = "/public/data/blog";
+const directory = path.join(process.cwd(), BLOG_DIRECTORY);
 
 export const getBlogList = async (): Promise<string[]> => {
-  const directory = path.join(process.cwd(), BLOG_DIRECTORY);
   const filenames = await readdir(directory);
-  const splitAndTakeFirst = compose<[string], string[], string>(
-    head,
-    split(".")
+
+  const splitAndTakeNth = (n: number) =>
+    compose<string[], string[], string>(nth(n), split("."));
+
+  const sortAndSelect = compose(
+    map(splitAndTakeNth(1)),
+    sortWith([descend(splitAndTakeNth(0))])
   );
-  const paths = map(splitAndTakeFirst, filenames);
-  return paths;
+
+  return sortAndSelect(filenames);
 };
 
 export const getBlogData = async (entry: string): Promise<BlogEntry> => {
   const title = getBlogTitleByEntry(entry);
-  const filename = path.join(process.cwd(), `${BLOG_DIRECTORY}/${entry}.md`);
+  const filenames = await readdir(directory);
+  const wantedFileName = filenames.filter(item => item.includes(entry));
+  const filePath = path.join(
+    process.cwd(),
+    `${BLOG_DIRECTORY}/${wantedFileName}`
+  );
 
-  const markdownText = await readFile(filename, "utf8");
+  const markdownText = await readFile(filePath, "utf8");
   marked.setOptions({ gfm: true });
   const content = marked.parse(markdownText);
 
